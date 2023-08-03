432d70c88be355502f45a147612604b6d40209ca
urmila1907 <urmilasangwan744@gmail.com>
2023-08-03 13:20:08 +0530

    Created user MVC and tested register api

---
+++ api/controllers/userCtrl.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+
+const createUser = async (req, res) => {
+  const email = req.body.email;
+  const findUser = await User.findOne({email:email});
+  if (!findUser) {
+    const newUser = User.create(req.body);
+    res.json(newUser);
+  } else {
+    res.json({
+      message: "User already exists",
+      success: false,
+    });
+  }
+};
+
+module.exports = createUser;
--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const dbConnect = require("./config/dbConnect");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute");
 
 dbConnect();
-app.use("/",(req,res)=>{
-    res.send("Hello from server side");
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/user", authRouter);
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening at ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
