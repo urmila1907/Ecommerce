6c6616ef5a7ef123ed4647f9230f1d7559701b58
urmila1907 <urmilasangwan744@gmail.com>
2023-08-03 13:20:53 +0530

    Small modification in userCtrl

--- api/controllers/userCtrl.js
+++ api/controllers/userCtrl.js
@@ -4,7 +4,7 @@ const createUser = async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({email:email});
   if (!findUser) {
-    const newUser = User.create(req.body);
+    const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
     res.json({
