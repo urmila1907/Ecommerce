c3f6367618bb2fc723338e6b5eda2d423921dbb3
urmila1907 <urmilasangwan744@gmail.com>
2023-08-18 15:38:25 +0530

    minute changes

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -12,42 +12,42 @@ import {Link} from "react-router-dom";
 export const ProductCard = () => {
   return (
     <>
-      <Link>
-        <div className="card">
-          <div className="product-image">
-            <img
-              src="images/watch.jpg"
-              className="card-img-top"
-              alt="blog-img"
-            />
-            <img
-              src="images/watch.avif"
-              className="card-img-top"
-              alt="blog-img"
-            />
-          </div>
-          <FavoriteBorderOutlined className="icons wishlist-icon" />
-          <div className="action-bar">
-            <div className="d-flex flex-column">
-              <Shuffle className="icons" />
-              <Visibility className="icons" />
-              <ShoppingBagOutlined className="icons" />
+        <Link>
+          <div className="card">
+            <div className="product-image">
+              <img
+                src="images/watch.jpg"
+                className="card-img-top"
+                alt="blog-img"
+              />
+              <img
+                src="images/watch.avif"
+                className="card-img-top"
+                alt="blog-img"
+              />
+            </div>
+            <FavoriteBorderOutlined className="icons wishlist-icon" />
+            <div className="action-bar">
+              <div className="d-flex flex-column">
+                <Shuffle className="icons" />
+                <Visibility className="icons" />
+                <ShoppingBagOutlined className="icons" />
+              </div>
+            </div>
+            <div className="content">
+              <h6 className="brand">Havells</h6>
+              <h5 className="product-title">Sony latest watch for men</h5>
+              <ReactStars
+                count={5}
+                size={24}
+                value={3}
+                edit={false}
+                activeColor="var(--star-color)"
+              />
+              <p className="price">$100.00</p>
             </div>
           </div>
-          <div className="content">
-            <h6 className="brand">Havells</h6>
-            <h5 className="product-title">Sony latest watch for men</h5>
-            <ReactStars
-              count={5}
-              size={24}
-              value={3}
-              edit={false}
-              activeColor="var(--star-color)"
-            />
-            <p className="price">$100.00</p>
-          </div>
-        </div>
-      </Link>
+        </Link>
     </>
   );
 };
