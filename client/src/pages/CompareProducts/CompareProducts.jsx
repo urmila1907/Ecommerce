ed016a13d53244f9ce8bc60e230bb60c4b09c036
urmila1907 <urmilasangwan744@gmail.com>
2023-09-01 18:23:02 +0530

    creating contact page and compare products page

---
+++ client/src/pages/CompareProducts/CompareProducts.jsx
@@ -0,0 +1,16 @@
+ import React from "react";
+ import "./compareProducts.scss";
+ import { BreadCrumb } from "../../components/BreadCrumb/BreadCrumb";
+ import { Meta } from "../../components/Meta";
+
+export const CompareProducts = () => {
+   return (
+     <>
+       <Meta title="Compare Products" />
+       <BreadCrumb title="Compare Products" />
+       
+     </>
+   );
+ }
+
+
